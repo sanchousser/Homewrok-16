f507f75acf4b5167be3304c156335ca1b56a0d22
DumLiviu <158556659+DumLiviu@users.noreply.github.com>
2024-04-18 23:57:08 +0300

    Update

---
+++ src/about-modal.js
@@ -0,0 +1,19 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
+
+    if (refs.openModalBtn && refs.closeModalBtn && refs.modal) {
+        refs.openModalBtn.addEventListener("click", toggleModal);
+        refs.closeModalBtn.addEventListener("click", toggleModal);
+    }
+
+    function toggleModal() {
+        if (refs.modal) {
+            refs.modal.classList.toggle("is-hidden");
+        }
+    }
+});
