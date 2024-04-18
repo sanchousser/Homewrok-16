59bba6040aa2d01e3b419cba9497346d4536db86
MatacheAlexandru <111695999+MatacheAlexandru@users.noreply.github.com>
2024-04-19 01:18:42 +0300

    commit 7 tasks

--- src/about-modal.js
+++ src/about-modal.js
@@ -1,19 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const refs = {
+    openModalBtn: document.getElementById('data-modal-open'),
+    closeModalBtn: document.getElementById('data-modal-close'),
+    modal: document.getElementById('data-modal'),
+  };
 
-document.addEventListener('DOMContentLoaded', function() {
-    const refs = {
-        openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector("[data-modal-close]"),
-        modal: document.querySelector("[data-modal]"),
-    };
+  if (refs.openModalBtn && refs.closeModalBtn && refs.modal) {
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  }
 
-    if (refs.openModalBtn && refs.closeModalBtn && refs.modal) {
-        refs.openModalBtn.addEventListener("click", toggleModal);
-        refs.closeModalBtn.addEventListener("click", toggleModal);
+  function toggleModal() {
+    if (refs.modal) {
+      refs.modal.classList.toggle('is-hidden-about');
     }
-
-    function toggleModal() {
-        if (refs.modal) {
-            refs.modal.classList.toggle("is-hidden");
-        }
-    }
+  }
 });
