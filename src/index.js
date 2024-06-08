ee4ad01c0b0e3fc1693c914002e63ee4a752ce89
roxana-toma <toma.iroxana@gmail.com>
2024-06-08 19:06:00 +0200

    hero-header 8June24 v2

--- src/index.js
+++ src/index.js
@@ -21,9 +21,9 @@
 
   const toggleMenu = () => {
     const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+      openMenuBtn.getAttribute('aria-expanded-header') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded-header', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open-header');
 
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
@@ -37,8 +37,8 @@
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
+    mobileMenu.classList.remove('is-open-header');
+    openMenuBtn.setAttribute('aria-expanded-header', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
