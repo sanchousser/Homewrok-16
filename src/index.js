8fe3bb624d55ca392ea62b14af84d071c32d7b6d
roxana-toma <toma.iroxana@gmail.com>
2024-06-11 00:11:30 +0200

    hh 11jun24 v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container-header');
+  const openMenuBtn = document.querySelector('.js-open-menu-header');
+  const closeMenuBtn = document.querySelector('.js-close-menu-header');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open-header');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open-header');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
 
 (() => {
   const refs = {
@@ -34,8 +62,12 @@ const refs = {
   openSellerModalBtn: document.querySelector('[data-modal-seller-open]'),
   openSellerModalBtnTab: document.querySelector('[data-modal-seller-open-tab]'),
   openSellerModalBtnDsk: document.querySelector('[data-modal-seller-open-dsk]'),
-  openSellerModalBtnHhDsk: document.querySelector('[data-modal-seller-open-hh-dsk]'),
-  openSellerModalBtnHhTab: document.querySelector('[data-modal-seller-open-hh-tab]'),
+  openSellerModalBtnHhDsk: document.querySelector(
+    '[data-modal-seller-open-hh-dsk]'
+  ),
+  openSellerModalBtnHhTab: document.querySelector(
+    '[data-modal-seller-open-hh-tab]'
+  ),
   openSellerModalBtnHh: document.querySelector('[data-modal-seller-open-hh]'),
   closeSellerModalBtn: document.querySelector('[data-modal-seller-close]'),
   sellerModal: document.querySelector('[data-modal-seller]'),
