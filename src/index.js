3917e8e7bd08bf3982ca42b7503c980fa72206e3
mserban27 <serban.mariamagdalena@yahoo.com>
2024-09-01 23:47:04 +0100

    Media queries 768px and 1200px + Modal box

--- src/index.js
+++ src/index.js
@@ -1,94 +1,72 @@
 // Section Contacts JS
 // opens modal window for franchise button
-
 const button = document.querySelector('.franchise-btn');
-const backdrop = document.querySelector('.franchise');
-
-button.addEventListener('click', toggleModal);
-button.addEventListener('click', function () {
-  console.log(13);
-});
-
-backdrop.addEventListener('click', e => {
-  const target = e.target.closest('.modal-close-btn');
+const franchiseBackdrop = document.querySelector('.franchise');
+button.addEventListener('click', toggleFranchiseModal);
+franchiseBackdrop.addEventListener('click', e => {
+  const target = e.target.closest('.modal-close-btn-contacts');
   if (!target) {
     return;
   }
-  toggleModal();
+  toggleFranchiseModal();
 });
-
-function toggleModal() {
-  backdrop.classList.toggle('is-open');
-  document.querySelector('.container').classList.toggle('opaq-bg');
+function toggleFranchiseModal() {
+  franchiseBackdrop.classList.toggle('is-open');
+  document.querySelector('.contact-container').classList.toggle('opaq-bg');
   document
     .querySelector('.contact-container')
     .scrollIntoView({ behavior: 'smooth' });
 }
-
 // opens modal window for locations button
-
 const locationBtn = document.querySelector('.location-btn');
 const locationBackdrop = document.querySelector('.location');
-
 locationBtn.addEventListener('click', toggleLocationModal);
 locationBtn.addEventListener('click', function () {
   console.log(2000);
 });
-
 locationBackdrop.addEventListener('click', e => {
-  const target = e.target.closest('.modal-close-btn');
+  const target = e.target.closest('.modal-close-btn-contacts');
   if (!target) {
     return;
   }
   toggleLocationModal();
 });
-
 function toggleLocationModal() {
   locationBackdrop.classList.toggle('is-open');
-  document.querySelector('.container').classList.toggle('opaq-bg');
+  document.querySelector('.contact-container').classList.toggle('opaq-bg');
   document
     .querySelector('.contact-container')
     .scrollIntoView({ behavior: 'smooth' });
 }
-
-
 //<--------------Customer Review JS----------->
-
 document.addEventListener('DOMContentLoaded', () => {
-  let currentSlideIndex = 0; 
+  let currentSlideIndex = 0;
   const slides = document.querySelectorAll('.carousel-item');
   const dots = document.querySelectorAll('.dot');
-
   function showSlide(index) {
     // Hide all slides and deactivate dots
     slides.forEach((slide, i) => {
       slide.classList.remove('active');
       dots[i].classList.remove('active');
     });
-
     // Show the selected slide and activate the dot
     slides[index].classList.add('active');
     dots[index].classList.add('active');
   }
-
   function nextSlide() {
     currentSlideIndex = (currentSlideIndex + 1) % slides.length;
     showSlide(currentSlideIndex);
   }
-
   function prevSlide() {
     currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
     showSlide(currentSlideIndex);
   }
-
   function goToSlide(index) {
     currentSlideIndex = index;
     showSlide(currentSlideIndex);
   }
-
   // Initialize the first slide
   showSlide(currentSlideIndex);
-
   // Assign event listeners to buttons
   document.querySelector('.prev-btn').addEventListener('click', prevSlide);
   document.querySelector('.next-btn').addEventListener('click', nextSlide);
@@ -96,3 +74,21 @@ document.addEventListener('DOMContentLoaded', () => {
     dot.addEventListener('click', () => goToSlide(index));
   });
 });
+// About section JS
+// opens modal window for read more button
+const aboutButton = document.querySelector('.read-more-btn');
+const aboutBackdrop = document.querySelector('.modal-backdrop');
+aboutButton.addEventListener('click', toggleModal);
+aboutButton.addEventListener('click', function () {
+  console.log(230924928);
+});
+aboutBackdrop.addEventListener('click', e => {
+  const target = e.target.closest('.close-modal-box');
+  if (!target) {
+    return;
+  }
+  toggleModal();
+});
+function toggleModal() {
+  aboutBackdrop.classList.toggle('is-open');
+}
